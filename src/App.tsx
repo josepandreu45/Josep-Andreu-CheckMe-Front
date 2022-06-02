3159acebe69991a8968e6d11f6ba86c9148364a4
josep andreu <josepa45@gmail.com>
2022-06-02 21:26:57 +0200

    Created Access control

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,37 @@
-import React from "react";
-import { Navigate, Route, Routes } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Navigate, Route, Routes, useNavigate } from "react-router-dom";
+import { AccesControlUnlogged } from "./components/AccesControl/AccesControl";
+import HomePage from "./pages/HomePage/HomePage";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
+import { useAppSelector } from "./redux/hooks/hooks";
 
 function App() {
+  const { logged } = useAppSelector((state) => state.user);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (logged) {
+      navigate("/home");
+    } else {
+      navigate("/login");
+    }
+  }, [navigate, logged]);
+
   return (
     <>
       <Routes>
         <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route
+          path="/home"
+          element={
+            <AccesControlUnlogged>
+              <HomePage />
+            </AccesControlUnlogged>
+          }
+        />
       </Routes>
     </>
   );
---
+++ src/components/AccesControl/AccesControl.tsx
@@ -0,0 +1,24 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAppSelector } from "../../redux/hooks/hooks";
+
+interface Children {
+  children: JSX.Element;
+}
+
+export const AccesControlUnlogged = ({ children }: Children) => {
+  const { logged } = useAppSelector((state) => state.user);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!logged) {
+      navigate("/login");
+    }
+  }, [logged, navigate]);
+
+  if (logged) {
+    return children;
+  } else {
+    return null;
+  }
+};
