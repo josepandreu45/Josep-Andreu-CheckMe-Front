84ed6b6d817b6e979c9df7f96665bf7193847859
josep andreu <josepa45@gmail.com>
2022-06-03 12:53:07 +0200

    implemented logout and fixed test failures

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,22 +3,42 @@ import { render, screen } from "@testing-library/react";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { store } from "./redux/store/store";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+
+jest.mock("jwt-decode", () => () => ({
+  id: "1",
+  name: "josep",
+  username: "josep",
+}));
+
+const mockUseNavigate = jest.fn();
+
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useNavigate: () => mockUseNavigate,
+}));
 
 describe("Given an App component", () => {
-  describe("When it's rendered", () => {
-    test("Then it should show an image", () => {
+  describe("When it's rendered and a user is logged", () => {
+    test("Then it should navigate to /home", () => {
+      const userMockSlice = createSlice({
+        name: "user",
+        initialState: { username: "josep", logged: true },
+        reducers: {},
+      });
+      const mockStore = configureStore({
+        reducer: { user: userMockSlice.reducer },
+      });
+
       render(
-        <Provider store={store}>
+        <Provider store={mockStore}>
           <BrowserRouter>
             <App />
           </BrowserRouter>
         </Provider>
       );
 
-      const receivedResult = screen.getByRole("img");
-
-      expect(receivedResult).toBeInTheDocument();
+      expect(mockUseNavigate).toHaveBeenCalledWith("/home");
     });
   });
 });
