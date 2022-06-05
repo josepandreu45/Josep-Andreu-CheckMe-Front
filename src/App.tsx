e0c9cfab259b0a40b3fb7b3a13ab425e229abb73
josep andreu <josepa45@gmail.com>
2022-06-05 19:33:50 +0200

    Deleted unused vars

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,6 @@ import { UserInfo } from "../src/types/userTypes";
 import Page404 from "./pages/Page404/Page404";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import Check from "./components/Check/Check";
 
 function App() {
   const { logged } = useAppSelector((state) => state.user);
