f6ccaa5009f40620cf65640aa867351e3e345e70
josep andreu <josepa45@gmail.com>
2022-06-07 14:07:09 +0200

    Created modal loading

--- src/modals/modals.ts
+++ src/modals/modals.ts
@@ -1,5 +1,10 @@
-import { toast } from "react-toastify";
+import { Id, toast } from "react-toastify";
+
+let idLoadingModal: Id;
 
 export const correctAction = (message: string) => toast.success(message);
 export const wrongAction = (message: string) => toast.error(message);
-export const loading = (message: string) => toast.loading(message);
+export const setLoadingOn = (message: string) => {
+  idLoadingModal = toast.loading(message);
+};
+export const setLoadingOff = () => toast.dismiss(idLoadingModal);
