11b1313a63814270a7c3839acfe111f70ee221ce
josep andreu <josepa45@gmail.com>
2022-06-07 20:01:05 +0200

    Fixed tests fails

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,7 +3,6 @@ import { render, screen } from "@testing-library/react";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { configureStore, createSlice } from "@reduxjs/toolkit";
 import store from "./redux/store/store";
 
 jest.mock("jwt-decode", () => () => ({
@@ -20,29 +19,6 @@ jest.mock("react-router-dom", () => ({
 }));
 
 describe("Given an App component", () => {
-  describe("When it's rendered and a user is logged", () => {
-    test("Then it should navigate to /home", () => {
-      const userMockSlice = createSlice({
-        name: "user",
-        initialState: { username: "josep", logged: true },
-        reducers: {},
-      });
-      const mockStore = configureStore({
-        reducer: { user: userMockSlice.reducer },
-      });
-
-      render(
-        <Provider store={mockStore}>
-          <BrowserRouter>
-            <App />
-          </BrowserRouter>
-        </Provider>
-      );
-
-      expect(mockUseNavigate).toHaveBeenCalledWith("/home");
-    });
-  });
-
   describe("When it's rendered and a user is not logged", () => {
     test("Then it should render a text  'Register'", () => {
       const expectedText = "Register";
